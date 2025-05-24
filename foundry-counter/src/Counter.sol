fb563df43901be55c4afbe0bd0a7d92cebbd733d
Evgeny Ukhanov <evgeny@aurora.dev>
2025-05-25 00:22:12 +0200

    XCC counter extended call

--- foundry-counter/src/Counter.sol
+++ foundry-counter/src/Counter.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.29;
 
 import "@openzeppelin-contracts/access/AccessControl.sol";
+import "@openzeppelin-contracts/utils/Strings.sol";
 import {
-AuroraSdk,
-IERC20,
-Codec,
-NEAR,
-PromiseCreateArgs,
-PromiseResultStatus,
-PromiseWithCallback
+    AuroraSdk,
+    IERC20,
+    Codec,
+    NEAR,
+    PromiseCreateArgs,
+    PromiseResultStatus,
+    PromiseWithCallback
 } from "@aurora-sdk/AuroraSdk.sol";
 
 // NEAR gas constants (TGas)
@@ -26,6 +27,7 @@ contract Counter is AccessControl {
     bytes32 public constant CALLBACK_ROLE = keccak256("CALLBACK_ROLE");
 
     uint256 public number;
+    uint256 public numberFromNEAR;
     string public nearAccountId;
     NEAR public near;
     IERC20 public wNEAR;
@@ -36,21 +38,6 @@ contract Counter is AccessControl {
         near = AuroraSdk.initNear(_wNEAR);
         wNEAR = _wNEAR;
         _grantRole(CALLBACK_ROLE, AuroraSdk.nearRepresentitiveImplicitAddress(address(this)));
-    }
-
-    function approveWNEAR() public {
-        uint256 amount = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-        PromiseCreateArgs memory approveCall = near.auroraCall(
-            address(this.wNEAR()),
-            abi.encodeWithSelector(
-                0x095ea7b3, // approve method selector
-                address(this),
-                amount
-            ),
-            0,
-            APPROVE_NEAR_GAS
-        );
-        approveCall.transact();
     }
 
     function setNumber(uint256 _number) public {
@@ -69,14 +56,50 @@ contract Counter is AccessControl {
     function nearIncCall() public {
         bytes memory data = "";
         PromiseCreateArgs memory callInc = near.call(nearAccountId, "inc", data, 0, CONTRACT_CALL_NEAR_GAS);
-        PromiseCreateArgs memory callback = near.auroraCall(address(this), abi.encodePacked(this.setCallback.selector), 0, CONTRACT_CALLBACK_NEAR_GAS);
+        PromiseCreateArgs memory callback =
+            near.auroraCall(address(this), abi.encodePacked(this.incCallback.selector), 0, CONTRACT_CALLBACK_NEAR_GAS);
 
         callInc.then(callback).transact();
     }
 
-    function setCallback() public onlyRole(CALLBACK_ROLE) {
+    function incCallback() public onlyRole(CALLBACK_ROLE) {
         if (AuroraSdk.promiseResult(0).status != PromiseResultStatus.Successful) {
             revert("Call to set failed");
         }
+    }
+
+    function nearIncByValueCall(uint256 _number) public {
+        bytes memory data = abi.encodePacked('{"value": "', Strings.toString(_number), '"}');
+        PromiseCreateArgs memory callInc = near.call(nearAccountId, "inc_by_value", data, 0, CONTRACT_CALL_NEAR_GAS);
+        PromiseCreateArgs memory callback =
+            near.auroraCall(address(this), abi.encodePacked(this.incCallback.selector), 0, CONTRACT_CALLBACK_NEAR_GAS);
+
+        callInc.then(callback).transact();
+    }
+
+    function nearGetCounterCall() public {
+        bytes memory data = "";
+        PromiseCreateArgs memory callInc = near.call(nearAccountId, "get_counter", data, 0, CONTRACT_CALL_NEAR_GAS);
+        PromiseCreateArgs memory callback = near.auroraCall(
+            address(this), abi.encodePacked(this.getCounterCallback.selector), 0, CONTRACT_CALLBACK_NEAR_GAS
+        );
+
+        callInc.then(callback).transact();
+    }
+
+    function getCounterCallback() public onlyRole(CALLBACK_ROLE) {
+        if (AuroraSdk.promiseResult(0).status != PromiseResultStatus.Successful) {
+            revert("Call to set failed");
+        }
+        numberFromNEAR = bytesToUint(AuroraSdk.promiseResult(0).output);
+    }
+
+    function bytesToUint(bytes memory b) public pure returns (uint256) {
+        require(b.length >= 32, "Input must be at least 32 bytes");
+        uint256 value;
+        assembly {
+            value := mload(add(b, 32))
+        }
+        return value;
     }
 }
